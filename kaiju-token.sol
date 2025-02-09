da0f645e8b2419f7f1a168d86a9c76104a163c60
mattkaiju <matt@kaiju.fi>
2025-02-09 17:23:31 +0800

    Update kaiju-token.sol

--- kaiju-token.sol
+++ kaiju-token.sol
@@ -1,6 +1,49 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.21.0;
+pragma solidity >=0.8.28.0;
 
-contract KaijuToken {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+contract KaijuToken is Initializable, ERC20Upgradeable, OwnableUpgradeable, ERC20PermitUpgradeable, UUPSUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(string memory name, string memory symbol, address initialOwner, uint256 initialSupply) initializer public {
+        __ERC20_init(name, symbol);
+        __Ownable_init(initialOwner);
+        __ERC20Permit_init("MyToken");
+        __UUPSUpgradeable_init();
+
+        _mint(msg.sender, initialSupply);
+    }
     
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public {
+        uint256 currentAllowance = allowance(account, msg.sender);
+        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
+
+        unchecked {
+            _approve(account, msg.sender, currentAllowance - amount);
+        }
+
+        _burn(account, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
 }
