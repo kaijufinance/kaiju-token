e91556632806a548c43ec97c3522cb0a66da872e
mattkaiju <matt@kaiju.fi>
2025-02-26 23:18:47 -0700

    Update kaiju-token.sol

--- kaiju-token.sol
+++ kaiju-token.sol
@@ -1,16 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.8.28.0;
+pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract KaijuToken is Initializable, ERC20Upgradeable, OwnableUpgradeable, ERC20PermitUpgradeable, UUPSUpgradeable {
+//KADO
+contract KaijuToken is Initializable, ERC20Upgradeable, AccessControlUpgradeable, OwnableUpgradeable, ERC20PermitUpgradeable, UUPSUpgradeable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
     
     event MintedEvent(address to, uint256 amount, uint256 time);
     event BurnedEvent(address from, uint256 amount, uint256 time);
+    event RoleUpdated(bytes32 role, address account, bool isGranted);
     
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -19,41 +24,60 @@ contract KaijuToken is Initializable, ERC20Upgradeable, OwnableUpgradeable, ERC2
 
     function initialize(string memory name, string memory symbol, address initialOwner, uint256 initialSupply) initializer public {
         __ERC20_init(name, symbol);
+        __AccessControl_init();
         __Ownable_init(initialOwner);
         __ERC20Permit_init(name);
         __UUPSUpgradeable_init();
 
-        mint(msg.sender, initialSupply);
+        _grantRole(DEFAULT_ADMIN_ROLE, initialOwner);
+        _grantRole(MINTER_ROLE, initialOwner);
+        _grantRole(BURNER_ROLE, initialOwner);
+
+        _mint(initialOwner, initialSupply);
     }
     
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
-
         emit MintedEvent(to, amount, block.timestamp);
     }
 
-    function burn(uint256 amount) public {
-        _burn(msg.sender, amount);
-
-        emit BurnedEvent(msg.sender, amount, block.timestamp);
-    }
-
-    function burnFrom(address account, uint256 amount) public {
-        uint256 currentAllowance = allowance(account, msg.sender);
+    function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
+        require(hasRole(BURNER_ROLE, msg.sender), "Must have burner role to burn");
+        uint256 currentAllowance = allowance(from, msg.sender);
         require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
 
         unchecked {
-            _approve(account, msg.sender, currentAllowance - amount);
+            _approve(from, msg.sender, currentAllowance - amount);
         }
 
-        _burn(account, amount);
+        _burn(from, amount);
+        emit BurnedEvent(from, amount, block.timestamp);
+    }
 
-        emit BurnedEvent(account, amount, block.timestamp);
+    function updateRoleAssignment(bytes32 role, address account, bool grantRole) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(role == MINTER_ROLE || role == BURNER_ROLE, "Invalid role");
+        
+        if (grantRole) {
+            _grantRole(role, account);
+        } else {
+            _revokeRole(role, account);
+        }
+        
+        emit RoleUpdated(role, account, grantRole);
     }
 
     function _authorizeUpgrade(address newImplementation)
         internal
-        onlyOwner
         override
+        onlyRole(DEFAULT_ADMIN_ROLE)
     {}
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(AccessControlUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
